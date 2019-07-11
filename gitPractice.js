753feb589b5fd1f6552fd4fd5b976e3f18e71c30
jalenwASC6 <jalenw44@nycischool.org>
2019-07-11 14:50:21 -0400

    Multiply Array Values

---
+++ gitPractice.js
@@ -0,0 +1,11 @@
+// function that multiplies all values in array by 2 
+// and prints the new array
+let arr = ["5", "10", "15", "20"];
+function multiplyArrayNumbers(arr) {
+let i = 0
+while (i < arr.length) {
+console.log(arr[i] * 2)
+i++;
+}
+}
+multiplyArrayNumbers(arr);
